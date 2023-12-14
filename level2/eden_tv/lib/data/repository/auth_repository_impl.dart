47f5284177a958618de599f7753d6d3d6479c1d8
Lazaro Scull <lazaro.scull@musala.com>
2023-12-14 22:47:54 +0200

    Adding auth repository

---
+++ level2/eden_tv/lib/data/repository/auth_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:EdenTV/core/error/failure.dart';
+import 'package:EdenTV/domain/repository/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthRepository extends AuthRepository {
+
+  final FirebaseAuth auth;
+
+  FirebaseAuthRepository(this.auth);
+
+  @override
+  bool isUserLoggedIn() => auth.currentUser != null;
+
+  @override
+  Future<Either<Failure, UserCredential>> loginUser(
+      String email, String password) async {
+    try {
+      final userCredentials = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return Right(userCredentials);
+    } catch (error) {
+      return Left(SomeSpecificError(""));
+    }
+  }
+
+  @override
+  void logoutUser() {
+    auth.signOut();
+  }
+}
