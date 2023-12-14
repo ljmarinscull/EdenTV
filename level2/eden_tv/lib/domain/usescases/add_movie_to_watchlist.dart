57eaf368383978d7115357856befa9ae140e8f72
Lazaro Scull <lazaro.scull@musala.com>
2023-12-15 01:21:02 +0200

    Updating add_movie_to_watchlist usescase

--- level2/eden_tv/lib/domain/usescases/add_movie_to_watchlist.dart
+++ level2/eden_tv/lib/domain/usescases/add_movie_to_watchlist.dart
@@ -1,13 +1,16 @@
 
 import '../entities/movie.dart';
+import '../repository/auth_repository.dart';
 import '../repository/movie_repository.dart';
 
 class AddMovieToWatchlist {
-  final MovieRepository repository;
+  final MovieRepository movieRepository;
+  final AuthRepository authRepository;
 
-  AddMovieToWatchlist(this.repository);
+  AddMovieToWatchlist(this.authRepository,this.movieRepository);
 
-  Future<void> call(String username, Movie movie) async {
-    await repository.addMovieToWatchlist(username, movie);
+  Future<void> call(Movie movie) async {
+    final username = authRepository.loggedUserUsername();
+    await movieRepository.addMovieToWatchlist(username, movie);
   }
 }
