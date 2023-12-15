2227dbf717f61584bcd0931ca14d6417894cf2b3
Lazaro Scull <lazaro.scull@musala.com>
2023-12-15 15:06:08 +0200

    Adding loggedUserUsernameProvider

--- level2/eden_tv/lib/presentation/providers/auth_provider.dart
+++ level2/eden_tv/lib/presentation/providers/auth_provider.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/repository/auth_repository_impl.dart';
 import '../../domain/repository/auth_repository.dart';
+import '../../domain/usescases/get_logged_user_username.dart';
 import '../../domain/usescases/is_user_logged.dart';
 import '../../domain/usescases/sign_out_user.dart';
 import '../../domain/usescases/signin_user.dart';
@@ -30,30 +31,39 @@ final signInUserProvider = Provider<SignInUser>((ref) {
   return SignInUser(repository);
 });
 
-final authNotifierProvider = StateNotifierProvider<AuthNotifier, UserCredential?>((ref) {
+final loggedUserUsernameProvider = Provider<GetLoggedUserUsername>((ref) {
+  final repository = ref.read(authRepositoryProvider);
+  return GetLoggedUserUsername(repository);
+});
+
+final authNotifierProvider = StateNotifierProvider<AuthNotifier, bool>((ref) {
   final isUserLogged = ref.read(isUserLoggedProvider);
   final signOutUser = ref.read(signOutUserProvider);
   final signInUser = ref.read(signInUserProvider);
+  final loggedUserUsername = ref.read(loggedUserUsernameProvider);
 
-  return AuthNotifier(isUserLogged, signInUser, signOutUser);
+  return AuthNotifier(isUserLogged, signInUser, signOutUser,loggedUserUsername);
 });
 
-class AuthNotifier extends StateNotifier<UserCredential?> {
+class AuthNotifier extends StateNotifier<bool> {
   final SignInUser _signInUser;
   final SignInOutUser _signInOutUser;
   final IsUserLogged _isUserLogged;
+  final GetLoggedUserUsername _loggedUserUsername;
 
-  AuthNotifier( this._isUserLogged, this._signInUser, this._signInOutUser,)
-      : super(null);
+  AuthNotifier(this._isUserLogged, this._signInUser, this._signInOutUser,this._loggedUserUsername)
+      : super(false);
 
   Future<void> signInUser(String username, String password) async {
     final moviesOrFailure = await _signInUser(username, password);
     moviesOrFailure.fold(
-            (error) => state = null,
+            (error) => state = false,
             (result) => state = result);
   }
 
   bool isUserLoggedIn() => _isUserLogged();
 
   void logOut() => _signInOutUser();
+
+  String loggedUserUsername() => _loggedUserUsername();
 }
